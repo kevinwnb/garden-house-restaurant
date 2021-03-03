7f8d11ea5e38df02e390a262a2d67574a46ae166
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-03-02 22:23:24 -0400

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,37 +4,29 @@ import $ from "jquery";
 
 class Home extends Component {
   componentDidMount() {
-    var myIndex = 0;
-    carousel();
-
-    function carousel() {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
-      myIndex++;
-      if (myIndex > x.length) {
-        myIndex = 1;
-      }
-      $(x[myIndex - 1]).fadeIn(1000);
-      setTimeout(carousel, 9000);
+    function arrowAnimation() {
+      $(".scroll-down-icon").animate(
+        { top: "+=20px", opacity: "1" },
+        500,
+        function () {
+          $(".scroll-down-icon").animate(
+            { top: "-=20px", opacity: "0" },
+            500,
+            function () {
+              arrowAnimation();
+            }
+          );
+        }
+      );
     }
+    arrowAnimation();
   }
   render() {
     return (
       <Fragment>
         <div className={`background`}>
-          <img
-            className={"mySlides"}
-            src={image}
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
-          <img
-            className={"mySlides"}
-            src={image}
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
+          <h1>Garden House Restaurant</h1>
+          <i class="scroll-down-icon fas fa-angle-down"></i>
         </div>
         <div style={{ height: "600px" }}></div>
       </Fragment>
