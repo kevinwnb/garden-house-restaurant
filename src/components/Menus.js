728e6646f1436165e9151004f8900cfb589ac40c
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-03-09 20:22:36 -0400

    Update

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,4 +1,5 @@
 import { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 import { Container, Table } from "reactstrap";
 //import image from "../assets/images/IMG_6243-1.jpg";
 //import $ from "jquery";
@@ -10,25 +11,46 @@ class WeeklySpecials extends Component {
         <section id={"menus"}>
           <Container>
             <h1>Menus</h1>
-            <h4 className={"w-100"}>
-              BREAKFAST Traditional Garden House Breakfast:
-            </h4>
-            <div className={"d-flex"}>
-              <p>
-                2 eggs any style, choice of bacon or sausage, panfries &
-                homemade toasted bread 8am-11am
-              </p>
-              <p className={"ml-auto"}>$9.99</p>
+            <div className={"nav"}>
+              <Link className={"active"} to={"/menus/breakfast"}>
+                Breakfast
+              </Link>
+              <Link to={"/menus/lunch"}>Lunch</Link>
+              <Link to={"/menus/catering"}>Catering</Link>
             </div>
-            <h4 className={"w-100"}>
-              BREAKFAST Traditional Garden House Breakfast:
-            </h4>
-            <div className={"d-flex"}>
-              <p>
-                2 eggs any style, choice of bacon or sausage, panfries &
-                homemade toasted bread 8am-11am
-              </p>
-              <p className={"ml-auto"}>$9.99</p>
+            <div className={"content"}>
+              <div className={"item"}>
+                <h4 className={"w-100"}>
+                  BREAKFAST Traditional Garden House Breakfast:
+                </h4>
+                <div className={"d-flex"}>
+                  <p>
+                    2 eggs any style, choice of bacon or sausage, panfries &
+                    homemade toasted bread 8am-11am
+                  </p>
+                  <p className={"ml-auto"}>$9.99</p>
+                </div>
+              </div>
+              <div className={"item"}>
+                <h4 className={"w-100"}>Cinnamon French Toast:</h4>
+                <div className={"d-flex"}>
+                  <p>prepared with our homemade bread</p>
+                  <div className={"ml-auto"}>
+                    <p className={"d-block"}>w/bacon $7.99</p>
+                    <p className={"d-block"}>w/sausage $7.20</p>
+                  </div>
+                </div>
+              </div>
+              <div className={"item"}>
+                <h4 className={"w-100"}>Pancakes:</h4>
+                <div className={"d-flex"}>
+                  <p>"Made From Scratch" Pancakes"</p>
+                  <div className={"ml-auto"}>
+                    <p className={"d-block"}>w/bacon $7.99</p>
+                    <p className={"d-block"}>add blueberries $1.99</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </Container>
         </section>
