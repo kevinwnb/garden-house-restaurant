222a1a9d83ed9f09357d06de9327b5c81c489ce1
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-03-15 11:59:05 -0300

    Update

--- src/components/MenusCatering.js
+++ src/components/MenusCatering.js
@@ -8,92 +8,162 @@ class MenusCatering extends Component {
   render() {
     return (
       <Fragment>
-        <div className={"content"}>
-          <div className={"item"}>
-            <h4 className={"w-100"}>Catering:</h4>
-            <div className={"d-flex"}>
-              <p>
-                2 eggs any style, choice of bacon or sausage, panfries &
-                homemade toasted bread 8am-11am
-              </p>
-              <p className={"ml-auto"}>$9.99</p>
+        <section id="menuscatering">
+          <div className={"content"}>
+            <div className={"nav"}>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"breakfast"}
+                className={"active"}
+              >
+                Breakfast Suggestions
+              </a>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"lunch"}
+              >
+                Luncheon Suggestions
+              </a>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"catering"}
+              >
+                On the Light Side
+              </a>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"catering"}
+              >
+                Dinner Suggestions
+              </a>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"catering"}
+              >
+                Party Trays
+              </a>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"catering"}
+              >
+                Homemade Desserts
+              </a>
+              <a
+                href={"javascript:void(0)"}
+                onClick={(e) => {
+                  this.swap(e);
+                }}
+                id={"catering"}
+              >
+                For the Budget Conscious
+              </a>
             </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>Cinnamon French Toast:</h4>
-            <div className={"d-flex"}>
-              <p>prepared with our homemade bread</p>
-              <div className={"ml-auto"}>
-                <p className={"d-block"}>w/bacon $7.99</p>
-                <p className={"d-block"}>w/sausage $7.20</p>
+            <div className={"item"}>
+              <h4 className={"w-100"}>Catering:</h4>
+              <div className={"d-flex"}>
+                <p>
+                  2 eggs any style, choice of bacon or sausage, panfries &
+                  homemade toasted bread 8am-11am
+                </p>
+                <p className={"ml-auto"}>$9.99</p>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>Cinnamon French Toast:</h4>
+              <div className={"d-flex"}>
+                <p>prepared with our homemade bread</p>
+                <div className={"ml-auto"}>
+                  <p className={"d-block"}>w/bacon $7.99</p>
+                  <p className={"d-block"}>w/sausage $7.20</p>
+                </div>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>Pancakes:</h4>
+              <div className={"d-flex"}>
+                <p>"Made From Scratch" Pancakes"</p>
+                <div className={"ml-auto"}>
+                  <p className={"d-block"}>w/bacon $7.99</p>
+                  <p className={"d-block"}>add blueberries $1.99</p>
+                </div>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>
+                Ham, Cheddar Cheese & Vegetable Omelette:
+              </h4>
+              <div className={"d-flex"}>
+                <p>with panfries & homemade toasted bread</p>
+                <p className={"ml-auto"}>$12.99</p>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>
+                Fresh Vegetable & Cheddar Cheese Omelette:
+              </h4>
+              <div className={"d-flex"}>
+                <p>with panfries & homemade toasted bread</p>
+                <p className={"ml-auto"}>$10.99</p>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>
+                Toasted Western & Cheddar Cheese Sandwich:
+              </h4>
+              <div className={"d-flex"}>
+                <p>with panfries</p>
+                <p className={"ml-auto"}>$9.49</p>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>Garden House Breakfast Bagel Deluxe:</h4>
+              <div className={"d-flex"}>
+                <p>Bacon, fried egg & cheddar cheese</p>
+                <div className={"ml-auto"}>
+                  <p className={"d-block"}>$4.99</p>
+                  <p className={"d-block"}>w/panfries $6.99</p>
+                </div>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>Fresh Whole Wheat Breakfast Wrap:</h4>
+              <div className={"d-flex"}>
+                <p>
+                  Scrambled egg, panfries, cheese & bacon in a whole wheat
+                  tortilla wrap
+                </p>
+                <p className={"ml-auto"}>$5.99</p>
+              </div>
+            </div>
+            <div className={"item"}>
+              <h4 className={"w-100"}>
+                Old Fashioned Oatmeal with Brown Sugar:
+              </h4>
+              <div className={"d-flex"}>
+                <p>and homemade toasted bread</p>
+                <p className={"ml-auto"}>$6.49</p>
               </div>
             </div>
           </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>Pancakes:</h4>
-            <div className={"d-flex"}>
-              <p>"Made From Scratch" Pancakes"</p>
-              <div className={"ml-auto"}>
-                <p className={"d-block"}>w/bacon $7.99</p>
-                <p className={"d-block"}>add blueberries $1.99</p>
-              </div>
-            </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>
-              Ham, Cheddar Cheese & Vegetable Omelette:
-            </h4>
-            <div className={"d-flex"}>
-              <p>with panfries & homemade toasted bread</p>
-              <p className={"ml-auto"}>$12.99</p>
-            </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>
-              Fresh Vegetable & Cheddar Cheese Omelette:
-            </h4>
-            <div className={"d-flex"}>
-              <p>with panfries & homemade toasted bread</p>
-              <p className={"ml-auto"}>$10.99</p>
-            </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>
-              Toasted Western & Cheddar Cheese Sandwich:
-            </h4>
-            <div className={"d-flex"}>
-              <p>with panfries</p>
-              <p className={"ml-auto"}>$9.49</p>
-            </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>Garden House Breakfast Bagel Deluxe:</h4>
-            <div className={"d-flex"}>
-              <p>Bacon, fried egg & cheddar cheese</p>
-              <div className={"ml-auto"}>
-                <p className={"d-block"}>$4.99</p>
-                <p className={"d-block"}>w/panfries $6.99</p>
-              </div>
-            </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>Fresh Whole Wheat Breakfast Wrap:</h4>
-            <div className={"d-flex"}>
-              <p>
-                Scrambled egg, panfries, cheese & bacon in a whole wheat
-                tortilla wrap
-              </p>
-              <p className={"ml-auto"}>$5.99</p>
-            </div>
-          </div>
-          <div className={"item"}>
-            <h4 className={"w-100"}>Old Fashioned Oatmeal with Brown Sugar:</h4>
-            <div className={"d-flex"}>
-              <p>and homemade toasted bread</p>
-              <p className={"ml-auto"}>$6.49</p>
-            </div>
-          </div>
-        </div>
+        </section>
       </Fragment>
     );
   }
